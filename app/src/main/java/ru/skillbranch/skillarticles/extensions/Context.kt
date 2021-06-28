7b469b5d144bcb471b1bea7fc852af8bc7647aae
K4sho <lolunoobkaa@yandex.ru>
2021-06-28 22:29:18 +0300

    update: Добавил MarkdownBuilder и начал реализацию спанов

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/Context.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/Context.kt
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 import android.util.TypedValue
+import androidx.annotation.AttrRes
 
 fun Context.dpToPx(dp: Int): Float {
     return TypedValue.applyDimension(
@@ -36,3 +37,13 @@ val Context.isNetworkAvailable: Boolean
             cm.activeNetworkInfo?.run { isConnectedOrConnecting } ?: false
         }
     }
+
+fun Context.attrValue(@AttrRes id: Int): Int {
+    val value = TypedValue()
+    if (theme.resolveAttribute(id, value, true)) {
+        value.data
+        return value.data
+    } else {
+        error("can not attribute for : $id")
+    }
+}
