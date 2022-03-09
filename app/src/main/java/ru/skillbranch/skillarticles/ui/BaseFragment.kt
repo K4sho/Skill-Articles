11e8b5f7112b87ad0b6e877d3e3a1c405f83852c
K4sho <lolunoobkaa@yandex.ru>
2022-03-09 19:56:08 +0300

    fix article ui

--- app/src/main/java/ru/skillbranch/skillarticles/ui/BaseFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/BaseFragment.kt
@@ -36,6 +36,8 @@ abstract class BaseFragment<S, T : BaseViewModel<S>, B : ViewBinding>(@LayoutRes
         viewModel.observeNotifications(root, root::renderNotification)
         viewModel.observeNavigation(root, root::handleNavigation)
         viewModel.observeState(viewLifecycleOwner, ::renderUi)
+
+        observeViewModelData()
     }
 
     override fun onDestroyView() {
